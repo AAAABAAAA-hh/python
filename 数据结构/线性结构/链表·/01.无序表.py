99649bc709944c8a174e6dfbb1504f9abdf5b0c4
python <fushennb@gmail.com>
2025-03-15 00:57:57 +0800

    python

--- 数据结构/线性结构/链表·/01.无序表.py
+++ 数据结构/线性结构/链表·/01.无序表.py
@@ -3,7 +3,10 @@ class Node(object):
         self.data = data
         self.next = None
 
-
+class listNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
 
